02d13da1ed3d42d98b140044fa62f16ddfe9c194
Nathan Fedzen <nfedzen@hotmail.com>
2020-12-10 10:18:48 -0700

    commit tree 793472d0ea6b28f752273553213bbf810dd6c8dd

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Company
     attr_accessor :name, :size
 
@@ -14,3 +16,30 @@ companies = [
 ]
 
 ### Your code below this
+
+
+def name_and_size companies_array
+    companies_array.each { |company| puts "#{company.name} - #{company.size.to_s}"}
+end
+
+def sizes companies_array
+    companies_array.reduce(0) { |sum_size, company| sum_size + company.size}
+end
+
+def over_100 companies_array
+    companies_array.filter { |company| company.size > 100}
+end
+
+def find_company companies_array, name
+    companies_array.find {|company| company.name == name}
+end
+
+def largest_company companies_array
+    companies_array.map(&:size).max   
+end
+
+def large_to_small companies_array
+    companies_array.sort_by {|company| -company.size}
+end
+binding.pry
+0
